cbe81718f505a3ebb9f9fd59760f18b46a547368
Björn De Craemer <bjorn.decraemer@yahoo.com>
2022-12-15 18:36:19 +0100

    fixed jaxb

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-15T18:18:12+0100",
+    date = "2022-12-15T18:35:46+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.1 (Oracle Corporation)"
 )
 @Component
